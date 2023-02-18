733c854df1d1ad37de38e308488dd8020f4c5dfc
Alejandro jose Ruzafa casas <aruzafa-@c3r8s6.42malaga.com>
2023-02-18 20:06:32 +0100

    fix overflow

--- libft/ft/ft_atoi.c
+++ libft/ft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: aruzafa- <aruzafa-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 16:18:02 by aruzafa-          #+#    #+#             */
-/*   Updated: 2022/04/25 11:54:55 by aruzafa-         ###   ########.fr       */
+/*   Updated: 2023/02/18 19:17:53 by aruzafa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,44 @@ static int	ft_isspace(const char c)
 		|| c == ' ' );
 }
 
+static int	ft_check_len(const char *str)
+{
+	int	len;
+	int	sign;
+
+	sign = *str == '-';
+	while (*str == '+' || *str == '-' || *str == '0')
+		str++;
+	len = ft_strlen(str);
+	if (sign == 1 && len > 10)
+		return (0);
+	else if (sign == 0 && len > 10)
+		return (-1);
+	return (1);
+}
+
+static int	validate_overflow(int sign, int res)
+{
+	if (sign == 1 && res < 0)
+		return (-1);
+	else if (sign == -1 && res > 0)
+		return (0);
+	return (res);
+}
+
 int	ft_atoi(const char *str)
 {
 	int	res;
 	int	sign;
+	int	len_overflow;
 
 	res = 0;
 	sign = 1;
 	while (ft_isspace(*str))
 		str++;
+	len_overflow = ft_check_len(str);
+	if (len_overflow <= 0)
+		return (len_overflow);
 	if (*str == '-' || *str == '+')
 	{
 		if (*str == '-')
@@ -40,9 +69,5 @@ int	ft_atoi(const char *str)
 		str++;
 	}
 	res *= sign;
-	if (sign == 1 && res < 0)
-		return (-1);
-	else if (sign == -1 && res > 0)
-		return (0);
-	return (res);
+	return (validate_overflow(sign, res));
 }
