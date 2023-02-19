9a087ac11db24f7ee0d47d1be040b92ddc9f98cf
Alejandro jose Ruzafa casas <aruzafa-@c3r8s4.42malaga.com>
2023-02-19 13:38:34 +0100

    pase norm

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: aruzafa- <aruzafa-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 13:14:00 by aruzafa-          #+#    #+#             */
-/*   Updated: 2023/02/19 13:28:22 by aruzafa-         ###   ########.fr       */
+/*   Updated: 2023/02/19 13:36:12 by aruzafa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,22 @@ int	free_all(t_list **instr_lst, t_stacks **stacks, int *parsed_args)
 	return (0);
 }
 
+static t_list	*get_instr_list(t_stacks *stacks, int *parsed_args, int argc)
+{
+	t_list	*instr_lst;
+
+	if (stacks->size_a <= 3)
+		instr_lst = ps_simple_resolver(stacks);
+	else if (stacks->size_a <= 5)
+		instr_lst = ps_less_5_resolver(stacks);
+	else
+	{
+		instr_lst = ps_lis_stack(stacks, parsed_args, argc);
+		ft_lstadd_back(&instr_lst, ps_greedy_sort(stacks));
+	}
+	return (instr_lst);
+}
+
 int	main(int argc, char **argv)
 {
 	int			*parsed_args;
@@ -54,15 +70,7 @@ int	main(int argc, char **argv)
 	if (!ordered(parsed_args, argc))
 	{
 		stacks = ps_create_stacks(argc, parsed_args);
-		if (stacks->size_a <= 3)
-			instr_lst = ps_simple_resolver(stacks);
-		else if (stacks->size_a <= 5)
-			instr_lst = ps_less_5_resolver(stacks);
-		else
-		{
-			instr_lst = ps_lis_stack(stacks, parsed_args, argc);
-			ft_lstadd_back(&instr_lst, ps_greedy_sort(stacks));
-		}
+		instr_lst = get_instr_list(stacks, parsed_args, argc);
 		ps_print_instr(instr_lst);
 	}
 	return (free_all(&instr_lst, &stacks, parsed_args));
